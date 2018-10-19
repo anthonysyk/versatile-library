4236111df2aa373e830bf36355846b656d928d4f
anthonysyk <anthony.ssiyk@gmail.com>
2018-10-19 16:10:37 +0200

    add runtime memory actor

---
+++ src/main/scala/versatile/memory/RuntimeMemoryActor.scala
@@ -0,0 +1,23 @@
+package versatile.memory
+
+import akka.actor.Actor
+import versatile.memory.RuntimeMemoryActor.MemoryTick
+
+class RuntimeMemoryActor extends Actor {
+  override def receive: Receive = {
+    case MemoryTick =>
+      // memory info in MB
+      val mb = 1024 * 1024
+      val runtime = Runtime.getRuntime
+      println("** Used Memory:  " + (runtime.totalMemory - runtime.freeMemory) / mb)
+      println("** Free Memory:  " + runtime.freeMemory / mb)
+      println("** Total Memory: " + runtime.totalMemory / mb)
+      println("** Max Memory:   " + runtime.maxMemory / mb)
+  }
+}
+
+object RuntimeMemoryActor {
+
+  case object MemoryTick
+
+}
